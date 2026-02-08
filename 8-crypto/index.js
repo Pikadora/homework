7e66062f7973c99527ae7c8c4f6acd55f203034f
Pikadora <dorapikadora@yandex.ru>
2026-02-08 16:08:32 +0500

    совпадение паролей

--- 8-crypto/index.js
+++ 8-crypto/index.js
@@ -5,7 +5,6 @@ function crypto (pass) {
     let cryptoPassArr = [passArr.slice(2,4),passArr.slice(0,2).reverse(),passArr.slice(-1),passArr.slice(5,7),passArr.slice(4,5)];
     let res = cryptoPassArr.flat().join('');
     
-    console.log(res);
     return res;
 }
 
@@ -13,12 +12,8 @@ function check (defPass, inPass){
     let defPassArr = defPass.split('');
     let defArr = [defPassArr.slice(2,4).reverse(),defPassArr.slice(0,2),defPassArr.slice(-1),defPassArr.slice(5,7),defPassArr.slice(4,5)];
     let defRes = defArr.flat().join('');
-    
-    console.log(defRes);
 
-    if (defRes === inPass) return true;
-    
-    return false;
+    return defRes === inPass;
 }
 
 let cryptoPass = crypto(passDefault);
