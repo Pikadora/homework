d360e1c654a683d525b3c31806eb4f250859a112
Pikadora <dorapikadora@yandex.ru>
2026-03-30 21:48:04 +0500

    reviu

--- 11-date-array/index.js
+++ 11-date-array/index.js
@@ -1,14 +1,14 @@
 let arr = ["10-02-2022", "тест", "11/12/2023", "00/13/2022", "41/12/2023"];
 
-function filerDate (array) {
-    let resArr = [];
-    array.forEach(element => {
-        let res = new Date(element) 
-        if(res instanceof Date && !isNaN(res.getTime())){
-            resArr.push(element)
-        }
+function filterDate (array) {
+    return array.filter(element => {
+        let res = new Date(element); 
+        return res instanceof Date && !isNaN(res.getTime())
+    })
+    .map(el => {
+            let date = new Date(el);
+            return date.toLocaleString('ru-RU').split(",")[0].toString();
     });
-    return resArr;
 }
 
-console.log(filerDate(arr));
+console.log(filterDate(arr));
